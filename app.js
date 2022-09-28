691f92f27cf4cd0b6f3480a504c0d920be0f60cc
Daniel Chukwudubem <princedaniel7890@gmail.com>
2022-09-28 18:38:51 +0100

    working on local storage for the items

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const submitTaskUI = document.getElementById('submitTask')
 const clearList = document.querySelector('.clearListBox')
 const listCollection = document.getElementById('list_collection')
 const warning = document.querySelector('.warning')
+const li = document.createElement('li')
 
 clearList.style.display = 'none'
 
@@ -16,35 +17,37 @@ loadEventListeners()
 // call all event listeners
 
 function loadEventListeners(){
+    document.addEventListener('DOMcontentLoaded', loadTasks)
+
     // add submit event
     document.getElementById('task-form').addEventListener('submit', inputTask)
 
     taskInputUI.addEventListener('mouseup', taskInput)
 
     clearList.addEventListener('click', clearAll)
+
 }
 // input task
 
 function inputTask(e){
     const taskInputUI = document.getElementById('taskInput')
     const clearList = document.querySelector('.clearListBox')
-    const li = document.createElement('li')
 
     if(taskInputUI.value === ''){
         console.log('add a task boss');
        
-            warnings()
+        warnings()
         
         li.style.display = 'none'
     }
     else {
         //     console.log('your task have been added')
-        // creating li elements starts here
+        //      creating li elements starts here
         // ===========================================================
         // const li = document.createElement('li')
 
         const h4 = document.createElement('h4')
-        // setTimeout(warnings(),300);
+
         const link = document.createElement('a')
 
         link.id = 'delete'
@@ -63,6 +66,7 @@ function inputTask(e){
         listCollection.appendChild(li)
 
         warningsU()
+
     }
     
     // creating li element ends here
@@ -72,9 +76,11 @@ function inputTask(e){
 
     clearList.style.display = 'block'
 
+    addToLocalStorage((taskInputUI.value))
 
 }
-
+// creating the clearAll funtion
+// ==========================================
 function clearAll(e) {
     while (listCollection.firstChild) {
        if (confirm('are you sure you want to clear list??')){
@@ -115,6 +121,56 @@ function listRemove(e) {
     e.preventDefault()
 }
 
+// adding items to local storage function
+// ============================================
+
+function addToLocalStorage(task){
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = []
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks(){
+    console.log('loaded')
+
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = []
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    
+    tasks.forEach(function(task){
+        const h4 = document.createElement('h4')
+
+        const link = document.createElement('a')
+
+        link.id = 'delete'
+
+        h4.appendChild(document.createTextNode(task))
+
+        li.appendChild(h4)
+
+        li.appendChild(link)
+
+
+        link.innerHTML = '<img src="/cancel.png">' 
+    
+        link.addEventListener('click', listRemove )
+
+        listCollection.appendChild(li)
+    });
+
+
+}
+
 function taskInput(e) {
     if (taskInputUI.value.length > 0){
         document.getElementById('newTask').style.transform = 'translateX(0px) translateY(-20px)';
@@ -127,6 +183,7 @@ function taskInput(e) {
     }
 
 }
+
 
 
     // }else if (taskInputUI.value.length <= 0 || taskInputUI.onfocus === false){
